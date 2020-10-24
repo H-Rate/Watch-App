f6eea4f1a2afcf699e5a662d9f59257cd61ab104
an23lm <anselmjosephs@gmail.com>
2020-10-25 03:04:41 +0530

    Initial Commit

---
+++ h-rate WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  h-rate WatchKit Extension
+//
+//  Created by Anselm Joseph on 25/10/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ h-rate WatchKit Extension/NotificationView.swift
@@ -0,0 +1,20 @@
+//
+//  NotificationView.swift
+//  h-rate WatchKit Extension
+//
+//  Created by Anselm Joseph on 25/10/20.
+//
+
+import SwiftUI
+
+struct NotificationView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct NotificationView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationView()
+    }
+}
