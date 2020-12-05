6cead9a42168ff9df8d92eae218277ad8a99e073
Artoo Deetoo <anselmjs@icloud.com>
2020-12-05 23:22:32 +0530

    Initial Commit

---
+++ hrate WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  hrate WatchKit Extension
+//
+//  Created by Anselm Joseph on 05/12/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ hrate WatchKit Extension/hrateApp.swift
@@ -0,0 +1,19 @@
+//
+//  hrateApp.swift
+//  hrate WatchKit Extension
+//
+//  Created by Anselm Joseph on 05/12/20.
+//
+
+import SwiftUI
+
+@main
+struct hrateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
